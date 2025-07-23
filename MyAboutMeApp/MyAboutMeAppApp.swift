51689f97f6a8b0f05be1067de8c18fa343267bda
Aishani-Basu <aishanir.basu@gmail.com>
2025-07-23 13:26:48 -0400

    Initial Commit

---
+++ MyAboutMeApp/MyAboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAboutMeAppApp.swift
+//  MyAboutMeApp
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MyAboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
