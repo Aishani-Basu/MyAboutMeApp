879979516f0eed3c72b007b5241ab8e88a784ada
Aishani-Basu <aishanir.basu@gmail.com>
2025-07-23 13:29:35 -0400

    Add ABOUT ME text

--- MyAboutMeApp/ContentView.swift
+++ MyAboutMeApp/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("ABOUT ME")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            Spacer()
         }
         .padding()
     }
